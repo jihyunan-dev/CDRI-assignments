45311fc9a4fcad8a5adcc50959cdfa78293a1b2c
Jihyun An <hwiyu25@gmail.com>
2025-04-04 01:31:19 +0900

    ui: GlobalStyle 적용
    - variable 추가
    - reset.css 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RootRouter } from './routes/RootRouter';
+import { GlobalStyle } from './styles/GlobalStyle';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
       <BrowserRouter>
         <RootRouter />
       </BrowserRouter>
