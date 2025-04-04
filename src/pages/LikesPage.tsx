dcd5c593a2de6547d1f81e4c4a124b0b1c581887
Jihyun An <hwiyu25@gmail.com>
2025-04-04 19:10:08 +0900

    feat: LikesPage UI 및 데이터 반영

--- src/pages/LikesPage.tsx
+++ src/pages/LikesPage.tsx
@@ -1,3 +1,34 @@
+import { BookListCount } from '@/components/BookListCount';
+import { EmptyState } from '@/components/EmptyState';
+import { Page } from '@/components/Page';
+import { Stack } from '@/components/Stack';
+import { Typography } from '@/components/Typography';
+import { BookListItem } from '@/components/bookListItem';
+import { useLikeStore } from '@/store/useLikeStore';
+
 export function LikesPage() {
-  return <>LIKES</>;
+  const { likes } = useLikeStore();
+  const totalLikes = likes.length;
+
+  return (
+    <Page>
+      <Stack as="section" dir="column" gap={24} width="100%">
+        <Typography.Title type="title2">내가 찜한 책</Typography.Title>
+        <Stack dir="column" gap={36} width="100%">
+          <BookListCount title="찜한 책" count={totalLikes} unitText="건" />
+          {totalLikes === 0 ? (
+            <Stack justify="center" align="center" width="100%">
+              <EmptyState message="찜한 책이 없습니다." />
+            </Stack>
+          ) : (
+            <Stack dir="column">
+              {likes.map((book) => (
+                <BookListItem key={`like-${book.isbn}`} book={book} />
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Stack>
+    </Page>
+  );
 }
