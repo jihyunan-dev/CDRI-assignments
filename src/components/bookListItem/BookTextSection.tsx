5a0c5da51f0b24595bc948967248bee449e2ceac
Jihyun An <hwiyu25@gmail.com>
2025-04-04 21:08:31 +0900

    fix: BookListItem - 누락된 가격 정보 추가

--- src/components/bookListItem/BookTextSection.tsx
+++ src/components/bookListItem/BookTextSection.tsx
@@ -6,12 +6,12 @@ import { useBookListItemContext } from './context';
 export function BookTextSection() {
   const {
     isOpen,
-    book: { title, contents, authors },
+    book: { title, contents, authors, price, sale_price: salePrice },
   } = useBookListItemContext();
 
   return (
-    <Stack as="section" dir="column" css={style}>
-      <Stack align="center" gap={16}>
+    <Stack as="section" align="center" dir={isOpen ? 'column' : 'row'} css={style}>
+      <Stack align="center" gap={16} width="100%">
         <Typography.Title type="title3" color="primary">
           {title}
         </Typography.Title>
@@ -19,6 +19,12 @@ export function BookTextSection() {
           {authors.join(', ')}
         </Typography.Body>
       </Stack>
+
+      {!isOpen && (
+        <Typography.Title type="title3" color="primary" className="book-price">
+          {salePrice === -1 ? price.toLocaleString() : salePrice.toLocaleString()}원
+        </Typography.Title>
+      )}
 
       {isOpen && (
         <Stack dir="column" gap={12} width="100%" className="book-content">
@@ -46,5 +52,10 @@ const style = css`
   .book-content-description {
     line-height: 16px;
     white-space: pre-wrap;
+  }
+
+  .book-price {
+    flex-shrink: 0;
+    margin-left: 22px;
   }
 `;
