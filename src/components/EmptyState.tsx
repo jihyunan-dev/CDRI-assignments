96e5bec2820f79b38c2d0db9925263c824003ae3
Jihyun An <hwiyu25@gmail.com>
2025-04-04 11:00:42 +0900

    feat: EmptyState 컴포넌트 생성
    - 리스트가 비어있을 때 노출할 컴포넌트

---
+++ src/components/EmptyState.tsx
@@ -0,0 +1,20 @@
+/**
+ * @component EmptyState
+ * @description 리스트가 비었을때 보여줄 컴포넌트
+ */
+import IconBook from '@/assets/icon-book.svg?react';
+import { Stack } from './Stack';
+import { Typography } from './Typography';
+
+type EmptyStateProps = {
+  message?: string;
+};
+
+export function EmptyState({ message }: EmptyStateProps) {
+  return (
+    <Stack dir="column" align="center" gap={24}>
+      <IconBook />
+      <Typography.Caption color="secondary">{message}</Typography.Caption>
+    </Stack>
+  );
+}
