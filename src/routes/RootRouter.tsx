9f0b42fdde477a0cd2f722888aacc3f5eca3e3cb
Jihyun An <hwiyu25@gmail.com>
2025-04-05 14:05:29 +0900

    feat: Route lazy import 적용

--- src/routes/RootRouter.tsx
+++ src/routes/RootRouter.tsx
@@ -1,15 +1,19 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router';
 import { NotFoundPage } from '@/pages/404';
-import { LikesPage } from '@/pages/LikesPage';
-import { SearchPage } from '@/pages/SearchPage';
 import { PATH } from './path';
+
+const SearchPage = lazy(() => import('@/pages/SearchPage').then((module) => ({ default: module.SearchPage })));
+const LikesPage = lazy(() => import('@/pages/LikesPage').then((module) => ({ default: module.LikesPage })));
 
 export function RootRouter() {
   return (
-    <Routes>
-      <Route path={PATH.home} element={<SearchPage />} />
-      <Route path={PATH.likes} element={<LikesPage />} />
-      <Route path={'*'} element={<NotFoundPage />} />
-    </Routes>
+    <Suspense fallback={<>Loading...</>}>
+      <Routes>
+        <Route path={PATH.home} element={<SearchPage />} />
+        <Route path={PATH.likes} element={<LikesPage />} />
+        <Route path={'*'} element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
